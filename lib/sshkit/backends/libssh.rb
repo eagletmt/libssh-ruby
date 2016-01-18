3f2ef5260345398c346fdfd32bdc6e5e96472864
Kohei Suzuki <eagletmt@gmail.com>
2016-01-18 22:20:42 +0900

    Use connection pool

--- lib/sshkit/backends/libssh.rb
+++ lib/sshkit/backends/libssh.rb
@@ -1,10 +1,17 @@
 require 'libssh'
 require 'sshkit/backends/abstract'
+require 'sshkit/backends/connection_pool'
 
 module SSHKit
   module Backend
     class Libssh < Abstract
       BUFSIZ = 16384
+
+      @pool = SSHKit::Backend::ConnectionPool.new
+
+      class << self
+        attr_accessor :pool
+      end
 
       private
 
@@ -12,40 +19,53 @@ module SSHKit
         output.log_command_start(cmd)
         cmd.started = true
 
-        session = LibSSH::Session.new
-        session.host = host.hostname
-        session.parse_config
-        session.add_identity('%d/id_ed25519')
+        with_session do |session|
+          channel = LibSSH::Channel.new(session)
+          channel.open_session do
+            channel.request_exec(cmd.to_command)
+            until channel.eof?
+              stdout_avail = channel.poll(timeout: 1)
+              if stdout_avail && stdout_avail > 0
+                buf = channel.read(BUFSIZ)
+                cmd.on_stdout(channel, buf)
+                output.log_command_data(cmd, :stdout, buf)
+              end
 
-        session.connect
-        if session.server_known != LibSSH::SERVER_KNOWN_OK
-          raise 'unknown host'
-        end
-        if session.userauth_publickey_auto != LibSSH::AUTH_SUCCESS
-          raise 'authorization failed'
-        end
-
-        channel = LibSSH::Channel.new(session)
-        channel.open_session do
-          channel.request_exec(cmd.to_command)
-          until channel.eof?
-            stdout_avail = channel.poll(timeout: 1)
-            if stdout_avail && stdout_avail > 0
-              buf = channel.read(BUFSIZ)
-              cmd.on_stdout(channel, buf)
-              output.log_command_data(cmd, :stdout, buf)
+              stderr_avail = channel.poll(stderr: true, timeout: 1)
+              if stderr_avail && stderr_avail > 0
+                buf = channel.read(BUFSIZ, stderr: true)
+                cmd.on_stderr(channel, buf)
+                output.log_command_data(cmd, :stderr, buf)
+              end
             end
 
-            stderr_avail = channel.poll(stderr: true, timeout: 1)
-            if stderr_avail && stderr_avail > 0
-              buf = channel.read(BUFSIZ, stderr: true)
-              cmd.on_stderr(channel, buf)
-              output.log_command_data(cmd, :stderr, buf)
+            cmd.exit_status = channel.get_exit_status
+            output.log_command_exit(cmd)
+          end
+        end
+      end
+
+      def with_session(&block)
+        entry = self.class.pool.checkout(host.hostname) do |hostname|
+          LibSSH::Session.new.tap do |session|
+            session.host = host.hostname
+            session.parse_config
+            session.add_identity('%d/id_ed25519')
+
+            session.connect
+            if session.server_known != LibSSH::SERVER_KNOWN_OK
+              raise 'unknown host'
+            end
+            if session.userauth_publickey_auto != LibSSH::AUTH_SUCCESS
+              raise 'authorization failed'
             end
           end
+        end
 
-          cmd.exit_status = channel.get_exit_status
-          output.log_command_exit(cmd)
+        begin
+          block.call(entry.connection)
+        ensure
+          self.class.pool.checkin(entry)
         end
       end
     end
