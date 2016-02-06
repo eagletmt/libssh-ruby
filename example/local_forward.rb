85246eb01d60662726d3f24c8f3f00763e031f2a
Kohei Suzuki <eagletmt@gmail.com>
2016-02-07 01:13:25 +0900

    Add open_forward example

---
+++ example/local_forward.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+require 'libssh'
+require 'socket'
+require 'thread'
+
+GC.stress = true
+
+ssh_host = 'rossmann'
+remote_host = 'localhost'
+remote_port = 5432
+local_port = 3000
+
+session = LibSSH::Session.new
+session.host = ssh_host
+session.parse_config
+session.add_identity('%d/id_ed25519')
+
+session.connect
+if session.server_known != LibSSH::SERVER_KNOWN_OK
+  raise 'server unknown'
+end
+if session.userauth_publickey_auto != LibSSH::AUTH_SUCCESS
+  raise 'authorization failed'
+end
+
+mutex = Mutex.new
+cv = ConditionVariable.new
+server_thread = Thread.start do
+  bufsiz = 16384
+  TCPServer.open(local_port) do |server|
+    mutex.synchronize { cv.signal }
+    socket = server.accept
+    channel = LibSSH::Channel.new(session)
+    channel.open_forward(remote_host, remote_port) do
+      reader = Thread.start do
+        begin
+          loop do
+            channel.write(socket.readpartial(bufsiz))
+          end
+        rescue EOFError
+        end
+        channel.send_eof
+      end
+
+      writer = Thread.start do
+        loop do
+          size = channel.poll
+          if size.nil?
+            break
+          end
+          data = channel.read(size)
+          socket.write(data)
+        end
+      end
+
+      reader.join
+      writer.join
+    end
+  end
+end
+
+mutex.synchronize { cv.wait(mutex) }
+system('psql', '-h', 'localhost', '-p', local_port.to_s, '-U', 'postgres', '-d', 'postgres', '-c', 'SELECT NOW()')
+server_thread.join
