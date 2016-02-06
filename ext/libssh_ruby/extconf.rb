d1b1af025e02b31279c6a8f361e15e7a41d32be6
Kohei Suzuki <eagletmt@gmail.com>
2016-02-07 05:18:53 +0900

    Abort extconf when required headers or libraries are missing

--- ext/libssh_ruby/extconf.rb
+++ ext/libssh_ruby/extconf.rb
@@ -6,9 +6,16 @@ else
   $CFLAGS << ' -Wall -W'
 end
 
-have_header('libssh/libssh.h')
-have_library('ssh')
-have_library('ssh_threads')
+unless have_header('libssh/libssh.h')
+  abort 'Cannot find libssh/libssh.h'
+end
+unless have_library('ssh')
+  abort 'Cannot find libssh'
+end
+unless have_library('ssh_threads')
+  abort 'Cannot find libssh_threads'
+end
+
 have_const('SSH_KEYTYPE_ED25519', 'libssh/libssh.h')
 
 create_makefile('libssh/libssh_ruby')
